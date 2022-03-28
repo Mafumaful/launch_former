3907b9858bc68df22882c1e9fbd01fb7002520f6
mafumaful <2993821152@qq.com>
2022-03-28 21:23:54 +0800

    default

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # launch_former
-通过C简易配置ROS的launch文件
+通过C简易配置ROS的launch文件。
+
+## 如何使用
+
+建立一个build文件，在build文件里面：
+
+```bash
+cmake .. 
+make
+./test1
+```
+即可。
+
+在source目录下面存放你的roslaunch的命令。
--- src/test1.cpp
+++ src/test1.cpp
@@ -1,17 +1,73 @@
 #include <iostream>
 #include <fstream>
-#include <vector>
+#include <sstream>
 #include <string.h>
 
 using namespace std;
 
 int main(int argc, char const *argv[])
 {
-    ifstream targetFile("/Users/mafumaful/code/C:C++/ROS tools/src/test1.cpp", ios::in);
+    char c;
+    int line = 1;
+    ifstream targetFile("../source/file.txt", ios::in);
+    ofstream outputFile("../source/output.launch", ios::trunc);
     if (!targetFile)
     {
         cout << "cannot load the file!" << endl;
     }
 
+    while (targetFile.get(c))
+    {
+        if (c == '\n')
+            line++;
+    }
+    targetFile.close();
+
+    ifstream target("../source/file.txt", ios::in);
+    // 重新定位到文件开头
+    target.seekg(0, ios::beg);
+
+    string str[line][3];
+    for (int i = 0; i < line; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            target >> str[i][j];
+        }
+    }
+
+    outputFile << "<launch>" << endl;
+    for (int i = 0; i < line; i++)
+    {
+        if (str[i][0] == "rosrun")
+        {
+            outputFile
+                << "    <node pkg="
+                << "\""
+                << str[i][1]
+                << "\" "
+                << "name=\""
+                << str[i][2]
+                << "_node\" "
+                << "type=\""
+                << str[i][2]
+                << "\" />"
+                << endl;
+        }
+        if (str[i][0] == "roslaunch")
+        {
+            outputFile
+                << "    <include file=\"$(find "
+                << str[i][1]
+                << ")/launch/"
+                << str[i][2]
+                << "\" />"
+                << endl;
+        }
+    }
+    outputFile << "</launch>" << endl;
+
+    target.close();
+    outputFile.close();
     return 0;
 }
