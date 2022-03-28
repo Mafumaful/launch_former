fa344a2e3f238433952d1c293a0119fcedba3d85
mafumaful <2993821152@qq.com>
2022-03-28 20:17:46 +0800

    the first edition

---
+++ src/test1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string.h>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    ifstream targetFile("/Users/bianshuhao/code/C:C++/ROS tools/src/test1.cpp", ios::in);
+    if (!targetFile)
+    {
+        cout << "cannot load the file!" << endl;
+    }
+
+    return 0;
+}
